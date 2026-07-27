4f053b568fce9b9eb4cad98bd429871a8e0cf5df
Micah <your-noreply-email@users.noreply.github.com>
2026-07-27 14:20:08 -0700

    Fixed switch bug

--- main.c
+++ main.c
@@ -11,17 +11,21 @@ void ui() {
         case '1':
             iadd_entry();
             ui();
+            break;
         case '2':
             isearch_entry();
             ui();
+            break;
         case '3':
             iremove_entry();
             ui();
+            break;
         case '5':
             return;
         default: 
             iinvalid_mode();
             ui();
+            break;
     }
 
 }
